9482bc1b31b49f5411efbffe17a2620a07140b49
Yixin Zhang <m.yixin.zhang@gmail.com>
2016-06-30 09:58:10 -0400

    Updated script to function like live_plot_combined.sh
    Automatically search for number of transcensions, also supports transcension
    number as argument.

--- saves_plot.sh
+++ saves_plot.sh
@@ -1,58 +1,80 @@
 #!/bin/bash
 
-data=stats_plot
+COL_TRANSCENDS=1
+COL_ASCENDS=2
+COL_HZE=3
+COL_LAST_TRANS=4
+COL_HS_PER_MIN=6
 
-cat <<EOF | gnuplot -p
-set xlabel "Ascensions"
-set ylabel "HS/min for Transcension"
+transcension=$1
+if [[ -n $transcension && ! $transcension =~ ^[0-9]+$ ]]; then
+    echo >&2 "Not a valid transcension number: '$transcension'"
+    exit 1
+fi
+
+file=./stats_plot
+[[ -f $file ]] || exit 1
+
+regex="^[0-9]"
+if [[ -n $transcension ]]; then
+    regex="^$transcension "
+fi
+
+plot_data=$(grep "$regex" "$file")
+transcensions=($(echo "$plot_data" | cut -d' ' -f 1 | sort -n | uniq))
+ascensions=($(echo "$plot_data" | cut -d' ' -f 2 | sort -n | uniq))
+
+if (( ${#transcensions[@]} == 0 )); then
+    echo >&2 "No transcensions found"
+    exit 1
+fi
+
+plot1="'-' u $COL_ASCENDS:$COL_HS_PER_MIN w lp title 'HS/min, \1 Transcensions' axes x1y1"
+plots="$plot1, "
+
+plot2="'-' u $COL_ASCENDS:$COL_HZE w lp title 'HZE, \1 Transcensions' axes x1y2"
+more_plots=() #("$plot2")
+for p in "${more_plots[@]}"; do
+    plots="$plots$p, "
+done
+
+plot_str="plot $(echo ${transcensions[@]} | \
+                 sed -e "s|\([0-9]\+\)|$plots|g" -e "s/, $//")"
+
+xrange_beg=${ascensions[0]}
+if (( $xrange_beg > 0 )); then
+    xrange_beg=$((xrange_beg - 1))
+fi
+xrange_end=$((${ascensions[-1]} + 1))
+
+title="${#transcensions[@]} Transcensions"
+if [[ -n $transcension ]]; then
+    title="Transcension $transcension"
+fi
+
+plot=$(cat <<EOF
+set title "$title"
 set grid
-set logscale y
-plot "<(grep ^1 \"$data\" | uniq)" u 2:6 w lp title "1st Trans", \
-     "<(grep ^2 \"$data\" | uniq)" u 2:6 w lp title "2nd Trans", \
-     "<(grep ^3 \"$data\" | uniq)" u 2:6 w lp title "3rd Trans"
-EOF
-
-cat <<EOF | gnuplot -p
 set xlabel "Ascensions"
-set ylabel "HS/min for Transcension"
-set grid
-#set logscale y
-plot "<(grep ^1 \"$data\" | uniq)" u 2:6 w lp title "1st Trans", \
-     "<(grep ^2 \"$data\" | uniq)" u 2:6 w lp title "2nd Trans", \
-     "<(grep ^3 \"$data\" | uniq)" u 2:6 w lp title "3rd Trans"
-EOF
-
-cat <<EOF | gnuplot -p
-set xlabel "Ascensions"
-set ylabel "Highest Zone"
-set grid
-plot "<(grep ^1 \"$data\" | uniq)" u 2:3 w lp title "1st Trans", \
-     "<(grep ^2 \"$data\" | uniq)" u 2:3 w lp title "2nd Trans", \
-     "<(grep ^3 \"$data\" | uniq)" u 2:3 w lp title "3rd Trans"
-EOF
-
-cat <<EOF | gnuplot -p
-set xlabel "Ascensions"
-set ylabel "HS/min"
-set logscale y
-plot "<(grep ^1 \"$data\" | uniq)" u 2:6 w lp title "1st Trans - T", \
-     "<(grep ^2 \"$data\" | uniq)" u 2:6 w lp title "2nd Trans - T", \
-     "<(grep ^3 \"$data\" | uniq)" u 2:6 w lp title "3rd Trans - T", \
-     "<(grep ^1 \"$data\" | uniq)" u 2:7 w lp title "1st Trans - A", \
-     "<(grep ^2 \"$data\" | uniq)" u 2:7 w lp title "2nd Trans - A", \
-     "<(grep ^3 \"$data\" | uniq)" u 2:7 w lp title "3rd Trans - A"
-EOF
-
-cat <<EOF | gnuplot -p
-set xlabel "Ascensions"
-set ylabel "Minutes"
-set y2label "Highest Zone"
+set ylabel "HS/min (Transcension)"
+set y2label "HZE"
 set y2tics
-set grid
-plot "<(grep ^1 \"$data\" | uniq)" u 2:5 axes x1y1 w lp title "1st Trans Time", \
-     "<(grep ^2 \"$data\" | uniq)" u 2:5 axes x1y1 w lp title "2nd Trans Time", \
-     "<(grep ^3 \"$data\" | uniq)" u 2:5 axes x1y1 w lp title "3rd Trans Time", \
-     "<(grep ^1 \"$data\" | uniq)" u 2:3 axes x1y2 w lp title "1st Trans HZE", \
-     "<(grep ^2 \"$data\" | uniq)" u 2:3 axes x1y2 w lp title "2nd Trans HZE", \
-     "<(grep ^3 \"$data\" | uniq)" u 2:3 axes x1y2 w lp title "3rd Trans HZE"
+set autoscale
+set xrange [$xrange_beg:$xrange_end]
+#set nokey
+set key left top
+$plot_str
 EOF
+)
+
+for transcends in ${transcensions[@]}; do
+    cols=$((COL_TRANSCENDS-1))
+    transcends_data=$(echo "$plot_data" | \
+                         sed -n "/^\([^ ]\+ \+\)\{$cols\}$transcends /p")
+    plot=$plot$'\n'$transcends_data$'\n'e
+    for p in "${more_plots[@]}"; do
+        plot=$plot$'\n'$transcends_data$'\n'e
+    done
+done
+
+echo "$plot"
