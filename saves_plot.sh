68e8093e1a88cebecd4105940ce42ff900eb464d
Yixin Zhang <m.yixin.zhang@gmail.com>
2016-06-30 11:51:08 -0400

    Added arguments for x interval, included HZE on second y-axis

--- saves_plot.sh
+++ saves_plot.sh
@@ -6,7 +6,28 @@ COL_HZE=3
 COL_LAST_TRANS=4
 COL_HS_PER_MIN=6
 
+usage() {
+    cat <<EOF
+Usage: $0 [TRANSCENSIONS] [XRANGE-BEG] [XRANGE-END]
+Outputs commands suitable for piping into gnuplot.
+TRANSCENSIONS can be a number to limit to viewing one transcension, or '*' for
+all transcensions.
+XRANGE-BEG and XRANGE-END changes the x interval of the plot.
+EOF
+}
+
+if [[ $1 =~ ^((--)?help|-h)$ ]]; then
+    usage
+    exit
+fi
+
 transcension=$1
+xrange_beg=$2
+xrange_end=$3
+
+if [[ $transcension =~ ^\*$ ]]; then
+    transcension=
+fi
 if [[ -n $transcension && ! $transcension =~ ^[0-9]+$ ]]; then
     echo >&2 "Not a valid transcension number: '$transcension'"
     exit 1
@@ -33,7 +54,7 @@ plot1="'-' u $COL_ASCENDS:$COL_HS_PER_MIN w lp title 'HS/min, \1 Transcensions' 
 plots="$plot1, "
 
 plot2="'-' u $COL_ASCENDS:$COL_HZE w lp title 'HZE, \1 Transcensions' axes x1y2"
-more_plots=() #("$plot2")
+more_plots=("$plot2")
 for p in "${more_plots[@]}"; do
     plots="$plots$p, "
 done
@@ -41,11 +62,15 @@ done
 plot_str="plot $(echo ${transcensions[@]} | \
                  sed -e "s|\([0-9]\+\)|$plots|g" -e "s/, $//")"
 
-xrange_beg=${ascensions[0]}
-if (( $xrange_beg > 0 )); then
-    xrange_beg=$((xrange_beg - 1))
+if [[ -z $xrange_beg ]]; then
+    xrange_beg=${ascensions[0]}
+    if (( $xrange_beg > 0 )); then
+        xrange_beg=$((xrange_beg - 1))
+    fi
 fi
-xrange_end=$((${ascensions[-1]} + 1))
+if [[ -z $xrange_end ]]; then
+    xrange_end=$((${ascensions[-1]} + 1))
+fi
 
 title="${#transcensions[@]} Transcensions"
 if [[ -n $transcension ]]; then
