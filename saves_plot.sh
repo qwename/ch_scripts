c8fa821417e3504c33173c7293994b1fff28dcae
Yixin Zhang <m.yixin.zhang@gmail.com>
2016-07-01 00:34:07 -0400

    Added plot for equivalent AS amount on second y-axis

--- saves_plot.sh
+++ saves_plot.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-COL_TRANSCENDS=1
-COL_ASCENDS=2
+COL_T=1
+COL_A=2
 COL_HZE=3
-COL_LAST_TRANS=4
-COL_HS_PER_MIN=6
+COL_LAST_T=4
+COL_LAST_A=5
+COL_HS_PER_MIN_T=6
+COL_HS_PER_MIN_A=7
+COL_HS_SAC=8
+COL_TOTAL_HS=9
+COL_CURRENT_AS=10
+COL_ADD_AS=11
 
 usage() {
     cat <<EOF
@@ -42,18 +48,34 @@ if [[ -n $transcension ]]; then
 fi
 
 plot_data=$(grep "$regex" "$file")
-transcensions=($(echo "$plot_data" | cut -d' ' -f 1 | sort -n | uniq))
-ascensions=($(echo "$plot_data" | cut -d' ' -f 2 | sort -n | uniq))
+transcensions=($(echo "$plot_data" | cut -d' ' -f $COL_T | sort -n | uniq))
+ascensions=($(echo "$plot_data" | cut -d' ' -f $COL_A | sort -n | uniq))
+addOneAS=$(echo "$plot_data" | \
+           cut -d' ' -f $COL_A,$COL_CURRENT_AS,$COL_ADD_AS | \
+           sort -k 1,1n -k 3,3n | \
+           sed -n '/^\([0-9]\+ \)\{2\}1$/p')
+addOneAS_x=($(echo "$addOneAS" | cut -d' ' -f 1))
+addOneAS_y=($(echo "$addOneAS" | cut -d' ' -f 2))
 
 if (( ${#transcensions[@]} == 0 )); then
     echo >&2 "No transcensions found"
     exit 1
 fi
 
-plot1="'-' u $COL_ASCENDS:$COL_HS_PER_MIN w lp title 'HS/min, \1 Transcensions' axes x1y1"
+plot1_cols="$COL_A:$COL_HS_PER_MIN_T"
+plot1_title="HS/min"
+plot1_label="HS/min (Transcension)"
+plot1="'-' u $plot1_cols w lp title '$plot1_title, \1 Transcensions' axes x1y1"
 plots="$plot1, "
 
-plot2="'-' u $COL_ASCENDS:$COL_HZE w lp title 'HZE, \1 Transcensions' axes x1y2"
+#plot2_cols="$COL_A:$COL_HZE"
+#plot2_title="HZE"
+#plot2_label="HZE"
+plot2_cols="$COL_A:$COL_CURRENT_AS"
+plot2_title="AS"
+plot2_label="Ancient Souls"
+plot2="'-' u $plot2_cols w lp title '$plot2_title, \1 Transcensions' axes x1y2"
+
 more_plots=("$plot2")
 for p in "${more_plots[@]}"; do
     plots="$plots$p, "
@@ -77,23 +99,32 @@ if [[ -n $transcension ]]; then
     title="Transcension $transcension"
 fi
 
+lines=
+for i in $(eval echo {0..$((${#addOneAS_x[@]}-1))}); do
+    x_coord=${addOneAS_x[$i]}
+    y_coord=${addOneAS_y[$i]}
+    lines="set arrow from second $x_coord,0 to second $x_coord,$y_coord nohead"$'\n'$lines
+    lines="set arrow from second $x_coord,$y_coord to second $xrange_end,$y_coord nohead"$'\n'$lines
+done
+
 plot=$(cat <<EOF
 set title "$title"
 set grid
 set xlabel "Ascensions"
-set ylabel "HS/min (Transcension)"
-set y2label "HZE"
+set ylabel "$plot1_label"
+set y2label "$plot2_label"
 set y2tics
 set autoscale
 set xrange [$xrange_beg:$xrange_end]
 #set nokey
 set key left top
+$lines
 $plot_str
 EOF
 )
 
 for transcends in ${transcensions[@]}; do
-    cols=$((COL_TRANSCENDS-1))
+    cols=$((COL_T-1))
     transcends_data=$(echo "$plot_data" | \
                          sed -n "/^\([^ ]\+ \+\)\{$cols\}$transcends /p")
     plot=$plot$'\n'$transcends_data$'\n'e
@@ -101,5 +132,6 @@ for transcends in ${transcensions[@]}; do
         plot=$plot$'\n'$transcends_data$'\n'e
     done
 done
+
 
 echo "$plot"
